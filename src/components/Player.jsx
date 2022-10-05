744af3b60118237bae70a8b21457f1ea3c040894
Marc-Web <marconieng@gmail.com>
2022-10-04 22:24:12 -0300

    Estilizando o componente player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "../css/player-style.css";
+import { FcHeadset } from 'react-icons/fc';
 
 class Player extends React.Component {
   render() {
     return (
       <div className="div-player">
-        <p>Tocando agora(Player)</p>
-        <p>Selecione um podcast para ouvir</p>
-        <audio controls src="https://storage.googleapis.com/golden-wind/nextlevelweek/05-podcastr/audios/opensource.m4a"></audio>
+        <p className="text-title"><FcHeadset /> Tocando agora</p>
+        <div  className="box-episode">
+          <p>Selecione um podcast para ouvir</p>
+        </div>
+        <audio controls src="https://storage.googleapis.com/golden-wind/nextlevelweek/05-podcastr/audios/opensource.m4a" className="player-bar"></audio>
       </div>
     )
   }
